8bd0f1ae8ec084942233f70dd603c0bedaf99c08
Clone888 <dwclone@gmail.com>
2023-09-22 10:56:15 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿int hour = 0;
+int min = 0;
+int sek = 0;
+
+
+
+while (hour < 24)
+{
+
+    if (sek == 60)
+
+    {       
+        if (min == 60)
+        {
+            hour++;
+            min = 0;
+        }
+
+        min++;
+        sek = 0;
+    }
+
+
+    if (hour < 10)
+    {
+        Console.Write("0" + hour);
+    }
+    else if (hour >= 10)
+    {
+        Console.Write(hour);
+    }
+
+    if (min < 10)
+    {
+        Console.Write(":0" + min);
+    }
+    else if (min >= 10)
+    {
+        Console.Write(":" + min);
+    }
+    if (sek < 10)
+    {
+        Console.WriteLine(":0" + sek);
+    }
+    else if (sek >= 10)
+    {
+        Console.WriteLine(":" + sek);
+    }
+
+
+
+
+    hour++;
+}
+
